b87e3c23a3febf7dbb2dd9b10c2e77317d95e892
kasparkadalipp <kaspar.kadalipp@gmail.com>
2024-04-18 22:23:10 +0300

    Add utils

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+tqdmFormat = "{l_bar}{bar}| {n_fmt}/{total_fmt} [time elapsed: {elapsed}]"
+
+
+def saveToFile(devices, fileName, dataFolderName=""):
+    if not dataFolderName.endswith("/"): dataFolderName += "/"
+    folderPath = f"./{dataFolderName}"
+    if not os.path.exists(folderPath):
+        os.makedirs(folderPath)
+    with open(folderPath + fileName, 'w+', encoding='utf8') as file:
+        json.dump(devices, file, indent=2, ensure_ascii=False)
