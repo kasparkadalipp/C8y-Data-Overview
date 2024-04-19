c69d1644b89fba11c18acc4a644b9734fb3ec0f9
kasparkadalipp <kaspar.kadalipp@gmail.com>
2024-04-19 21:54:47 +0300

    Fix saved data path

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ tqdmFormat = "{l_bar}{bar}| {n_fmt}/{total_fmt} [time elapsed: {elapsed}]"
 
 def saveToFile(devices, fileName, dataFolderName=""):
     if dataFolderName and not dataFolderName.endswith("/"): dataFolderName += "/"
-    folderPath = f"..data/{dataFolderName}"
+    folderPath = f"../data/{dataFolderName}"
     if not os.path.exists(folderPath):
         os.makedirs(folderPath)
     with open(folderPath + fileName, 'w+', encoding='utf8') as file:
