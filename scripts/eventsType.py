a48fb6461b0af3c51dba0dd093d59268e91ebe30
kasparkadalipp <kaspar.kadalipp@gmail.com>
2024-04-23 00:48:14 +0300

    Add scripts for requesting monthly data

---
+++ scripts/eventsType.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+
+load_dotenv('../.env.telia')
+from src.cumulocity import MonthlyEvents
+from src.utils import tqdmFormat, saveToFile, pathExists, fileContentsFromFolder
+from tqdm import tqdm
+import json
+import calendar
+from datetime import date
+from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
+
+with open('../data/telia/c8y_data.json', 'r', encoding='utf8') as json_file:
+    c8y_data = json.load(json_file)
+
+files = fileContentsFromFolder('../data/telia/events/total')
+
+eventTypesMapping = {device['id']: set() for device in c8y_data}
+
+for jsonFile in files:
+    for device in jsonFile:
+        deviceId = device['deviceId']
+        event = device['total']['event']
+        if event:
+            eventTypesMapping[deviceId].add(event['type'])
+
+
+def requestFragmentSeries(year, month):
+    result = []
+    for device in tqdm(c8y_data, desc=f"{calendar.month_abbr[month]} {year}", bar_format=tqdmFormat):
+        deviceId = device['id']
+
+        c8y_measurements = {
+            "deviceId": deviceId,
+            "deviceType": device['type'],
+            "eventByType": []
+        }
+
+        for eventType in eventTypesMapping[deviceId]:
+            response = MonthlyEvents(device, enforceBounds=True).requestEventCountForType(year, month, eventType)
+
+            c8y_measurements['eventByType'].append({
+                "type": eventType,
+                "count": response['count'],
+                "event": response['event']
+            })
+        result.append(c8y_measurements)
+    return result
+
+
+print(f'Oldest event {min([parse(d['oldestEvent']['time']).date() for d in c8y_data if d['oldestEvent']])}')
+print(f'Latest event {max([parse(d['latestEvent']['time']).date() for d in c8y_data if d['latestEvent']])}')
+
+startingDate = date(2016, 11, 1)
+lastDate = date(2024, 3, 1)
+
+while startingDate <= lastDate:
+    year = startingDate.year
+    month = startingDate.month
+
+    filePath = f"telia/events/type/{MonthlyEvents.fileName(year, month)}"
+    if pathExists(filePath):
+        print(f"{calendar.month_abbr[month]} {year} - skipped")
+    else:
+        data = requestFragmentSeries(year, month)
+        saveToFile(data, filePath, overwrite=False)
+    startingDate += relativedelta(months=1)
