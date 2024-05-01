5ce037927e9e3eef5e015c10144ea41c018b31eb
kasparkadalipp <kaspar.kadalipp@gmail.com>
2024-05-01 10:41:20 +0300

    Update event schema

--- scripts/event jsonSchema.py
+++ scripts/event jsonSchema.py
@@ -16,7 +16,6 @@ def createSchema(event):
     return jsonSchema
 
 
-
 folder = "telia/events/type/"
 
 eventTypeMapping = defaultdict(lambda: {'schema': SchemaBuilder(schema_uri=False), 'count': 0, 'example': {}})
@@ -25,21 +24,29 @@ for fileName in fileNamesInFolder('../data/' + folder):
     for event in readFile(folder + fileName):
         eventTypeSum = 0
         deviceId = event['deviceId']
+        deviceType = event['deviceType']
         for eventTypeObj in event['eventByType']:
             eventType = eventTypeObj['type']
             event = eventTypeObj['event']
             count = eventTypeObj['count']
             if event:
                 jsonSchema = createSchema(event)
-                eventTypeMapping[eventType]['schema'].add_schema(jsonSchema)
-                eventTypeMapping[eventType]['count'] += count
-                eventTypeMapping[eventType]['example'] = event
+                key = (deviceType, eventType)
+                eventTypeMapping[key]['schema'].add_schema(jsonSchema)
+                eventTypeMapping[key]['count'] += count
+                eventTypeMapping[key]['example'] = event
 
 data = []
-for eventType, values in eventTypeMapping.items():
-    row = {'eventType': eventType, 'count': values['count'],
-           'jsonSchema': str(values['schema'].to_schema()).replace("'", '"'), 'example event': values['example']}
+for key, values in eventTypeMapping.items():
+    eventType, deviceType = key
+    row = {
+        'deviceType': deviceType,
+        'eventType': eventType,
+        'count': values['count'],
+        'jsonSchema': str(values['schema'].to_schema()).replace("'", '"'),
+        'example event': values['example']
+    }
     data.append(row)
 
 df = pd.DataFrame(data)
-df.to_csv("../data/telia/Event jsonSchema.csv", index=False, encoding='utf-8-sig')
+df.to_csv("../data/telia/Events.csv", index=False, encoding='utf-8-sig')
