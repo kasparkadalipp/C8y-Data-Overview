46fbbc1805cc48406828ea267e158ab73ee614fc
kasparkadalipp <kaspar.kadalipp@gmail.com>
2024-05-22 11:05:39 +0300

    Move _init_.py relative imports

--- src/cumulocity/__init__.py
+++ src/cumulocity/__init__.py
@@ -2,3 +2,14 @@ from .config import getCumulocityApi
 from .inventory import Inventory
 from .events import Events, MonthlyEvents
 from .measurements import Measurements, MonthlyMeasurements, requestMonthBounds
+
+from .requests.groups import getDeviceGroups
+from .requests.devices import Devices
+
+from .requests.eventsTotal import requestMonthlyData as eventsTotal
+from .requests.eventsType import requestMonthlyData as eventsType
+from .requests.eventsTypeFragment import requestMonthlyData as eventsTypeFragment
+
+from .requests.measurementTotal import requestMonthlyData as measurementsTotal
+from .requests.measurementFragmentSeries import requestMonthlyData as measurementsFragmentSeries
+from .requests.measurementTypeFragmentSeries import requestMonthlyData as measurementTypeFragmentSeries
