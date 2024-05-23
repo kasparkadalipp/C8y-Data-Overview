b3021c0eec4328ab647baa01b820dc654de38a45
kasparkadalipp <kaspar.kadalipp@gmail.com>
2024-05-23 11:25:26 +0300

    Rename files

---
+++ src/topic_model/berttopic_hierarchical_topic_model.py
@@ -0,0 +1,54 @@
+import os
+from skllm.config import SKLLMConfig
+
+SKLLMConfig.set_openai_key(os.getenv('OPENAI_API_KEY'))
+SKLLMConfig.set_openai_org(os.getenv('OPENAPI_ORGANIZATION_ID'))
+from src.utils import saveToFile, readFile
+from bertopic.representation import OpenAI as BertOpenAI
+from bertopic import BERTopic
+
+def createTopicModel():
+    inputData = readFile('chatGPT input.json')
+    docs = [str(device) for device in inputData.values()]
+    client = BertOpenAI()
+
+    prompt = """
+    I have a topic that contains the following documents: 
+    [DOCUMENTS]
+    The topic is described by the following keywords: [KEYWORDS]
+    
+    Based on the information above, extract a short but highly descriptive topic label of at most 5 words. Make sure it is in the following format:
+    topic: <topic label>
+    """
+
+    openai_model = BertOpenAI(client, model="gpt-3.5-turbo", exponential_backoff=True, chat=True, prompt=prompt)
+
+    topic_model = BERTopic(representation_model={"OpenAI": openai_model}, nr_topics="auto")
+    topics, probs = topic_model.fit_transform(docs)
+    hierarchical_topics = topic_model.hierarchical_topics(docs)
+
+
+    deviceMapping = {deviceId: str(topicId) for deviceId, topicId in zip(docs, topics)}
+    saveToFile(deviceMapping, 'visualisations/deviceID topicID mapping.json', True)
+    labelMapping = {topic: " | ".join(list(zip(*values))[0]) for topic, values in
+                    topic_model.topic_aspects_["OpenAI"].items()}
+    nameMapping = {}
+    parentMapping = {}
+    rootNode = -1
+    for _, row in hierarchical_topics.iterrows():
+        parent = int(row['Parent_ID'])
+        leftChild = int(row['Child_Left_ID'])
+        rightChild = int(row['Child_Right_ID'])
+        rootNode = max(rootNode, parent)
+        # nodeTopics = row['Topics']
+
+        nameMapping[parent] = labelMapping.get(parent, 'default')
+        parentMapping[leftChild] = str(parent)
+        parentMapping[rightChild] = str(parent)
+    for topicId, label in labelMapping.items():
+        nameMapping[topicId] = labelMapping[topicId]
+    rootNode = str(rootNode)
+    parentMapping["-1"] = rootNode
+    nameMapping[rootNode] = "Topic model root"
+    nodeMapping = {'root': rootNode, 'name': nameMapping, 'parent': parentMapping}
+    saveToFile(nodeMapping, 'visualisations/topic model.json', True)
---
+++ src/topic_model/chatgpt_describe_data.py
@@ -0,0 +1,66 @@
+import instructor
+import pandas as pd
+import os
+from openai import OpenAI
+from pydantic import BaseModel
+from skllm.config import SKLLMConfig
+from src.utils import tqdmFormat, getPath, readFile
+from tqdm import tqdm
+
+
+class Measurement(BaseModel):
+    description: str
+    domain: str
+    subdomain: str
+
+
+def requestChatGPTDescription(client, message_data, model):
+    return client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content":
+                    'Take on the persona of a data analyst who is proficient in interpreting JSON objects and '
+                    'extracting meaningful insights from them. '
+                    'The user will provide JSON objects representing measurement data from a smart city IoT device.'
+
+            },
+            {
+                "role": "user",
+                "content": 'Ignore device specific information and concisely summarise what kind of data is being sent. '
+                           'Avoid generic terms like "IoT" and "smart city". '
+                           'Also provide example of a smart city domain this device belongs to.'
+                           f"{message_data} "
+            }
+        ],
+        max_retries=5,
+        response_model=Measurement
+    )
+
+
+def transformData( model="gpt-4-turbo"):  # "gpt-3.5-turbo"
+    inputData = readFile('chatGPT input.json'),
+
+    SKLLMConfig.set_openai_key(os.getenv('OPENAI_API_KEY'))
+    SKLLMConfig.set_openai_org(os.getenv('OPENAPI_ORGANIZATION_ID'))
+    client = instructor.patch(OpenAI())
+
+    result = {}
+    failedRequests = []
+    for deviceId, data in tqdm(inputData.items(), desc="requesting data for gpt-3.5-turbo", bar_format=tqdmFormat):
+        try:
+            response = requestChatGPTDescription(client, data, model)
+            result[deviceId] = {
+                'id': deviceId,
+                'name': data['device'],
+                'domain': response.domain,
+                'description': response.description,
+                'isAggregated': response.isAggregated,
+                'input': data
+            }
+        except:
+            failedRequests.append(deviceId)
+
+    df = pd.DataFrame(result.values())
+    df.to_csv(getPath(f"{model} description.csv"), encoding='utf-8-sig', index=False)
