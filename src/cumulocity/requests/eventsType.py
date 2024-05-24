0c4ed6eb50c725b61e0234a2d1463671177eccbf
kasparkadalipp <kaspar.kadalipp@gmail.com>
2024-05-24 22:49:20 +0300

    Combine requesting types

--- src/cumulocity/requests/eventsType.py
+++ src/cumulocity/requests/eventsType.py
@@ -2,7 +2,7 @@ import calendar
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from src.cumulocity.requests.mapping.eventTypeMapping import createEventTypeMapping
-from src.cumulocity import MonthlyEvents
+from src.cumulocity import MonthlyEvents, getCumulocityApi
 from src.utils import tqdmFormat, saveToFile, pathExists, readFile, ensureFileAndRead
 from tqdm import tqdm
 
@@ -74,6 +74,55 @@ def requestEventTypes(year, month):
     return result
 
 
+def requestMissingTypesForMonth(year, month):
+    validationFilePath = f"events/total/{MonthlyEvents.fileName(year, month)}"
+    filePath = f"events/type/{MonthlyEvents.fileName(year, month)}"
+    typeMapping = ensureFileAndRead('events/c8y_events_id_to_type_mapping.json', createEventTypeMapping)
+
+    c8y = getCumulocityApi()
+    validation = {item['deviceId']: item['total']['count'] for item in readFile(validationFilePath)}
+
+    c8y_events = []
+    newTypes = False
+    for device in readFile(filePath):
+        deviceId = device['deviceId']
+        currentCount = 0
+        expectedCount = validation[deviceId]
+        for event in device['eventByType']:
+            currentCount += event['count']
+        if currentCount == expectedCount:
+            c8y_events.append(device)
+            continue
+
+        knownTypes = set(typeMapping[deviceId])
+        generator = c8y.events.select(source=deviceId, page_size=1000)
+        for event in generator:
+            eventType = event.type
+            if eventType not in knownTypes:
+                response = MonthlyEvents({'id': deviceId}, enforceBounds=False).requestEventCountForType(year, month,
+                                                                                                         eventType)
+                while response['count'] < 0:
+                    response = MonthlyEvents({'id': deviceId}, enforceBounds=False).requestAggregatedEventCountForType(
+                        year, month, eventType)
+
+                device['eventByType'].append({
+                    "type": eventType,
+                    "count": response['count'],
+                    "event": response['event']
+                })
+                currentCount += response['count']
+                knownTypes.add(eventType)
+                typeMapping[deviceId].append(eventType)
+                newTypes = True
+            if currentCount >= expectedCount:
+                break
+        c8y_events.append(device)
+
+    if newTypes:
+        saveToFile(typeMapping, 'events/c8y_events_id_to_type_mapping.json')
+        saveToFile(c8y_events, filePath)
+
+
 def requestMonthlyData(startingDate: date, lastDate: date):
     if startingDate <= lastDate:
         raise ValueError("Last date can't be before starting date")
@@ -97,32 +146,6 @@ def requestMonthlyData(startingDate: date, lastDate: date):
             saveToFile(data, filePath)
         elif fileExists:
             print(f"{calendar.month_abbr[month]} {year} - skipped")
-
-        currentDate -= relativedelta(months=1)
-
-
-def requestMissingTypes(startingDate: date, lastDate: date):
-    if startingDate <= lastDate:
-        raise ValueError("Last date can't be before starting date")
-
-    startingDate = startingDate.replace(day=1)
-    lastDate = lastDate.replace(day=1)
-    currentDate = startingDate
-    while lastDate <= currentDate <= startingDate:
-        year = currentDate.year
-        month = currentDate.month
-
-        filePath = f"events/type/{MonthlyEvents.fileName(year, month)}"
-        fileExists = pathExists(filePath)
-
-        if not fileExists:
-            data = requestMissingTypesForMonth(year, month)
-            saveToFile(data, filePath)
-
-        data = requestMissingValues(year, month, filePath)
-        if data:
-            saveToFile(data, filePath)
-        elif fileExists:
-            print(f"{calendar.month_abbr[month]} {year} - skipped")
+        requestMissingTypesForMonth(year, month)
 
         currentDate -= relativedelta(months=1)
